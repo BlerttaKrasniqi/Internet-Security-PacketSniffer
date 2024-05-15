33d03b5007c52aadb8dd892512c1a7baec499ae3
diartmaraj <118026687+diartmaraj@users.noreply.github.com>
2024-05-15 17:05:54 +0200

    PS-3-Create-main-function:Modified the main function code

--- PacketSniffer.py
+++ PacketSniffer.py
@@ -3,10 +3,12 @@ import struct
 import textwrap
 
 def main():
-    conncetion = socket.socket(socket.AF_PACKET, socket.SOCK_RAW, socket.ntohs(3))
+    connection = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_IP)
+    connection.bind(('0.0.0.0', 0))
+    connection.setsockopt(socket.IPPROTO_IP, socket.IP_HDRINCL, 1)
     
     while True:
-        raw_data, addr = conncetion.recvfrom(65536)
+        raw_data, addr = connection.recvfrom(65536)
         dest_mac, src_mac, eth_protocol, data = ethernet_frame(raw_data)
         print('\nEthernet frame:')
         print('Destination: {}, Source: {}, Protocol: {},'. format(dest_mac, src_mac, eth_protocol))
@@ -24,7 +26,8 @@ def get_mac_addr(bytes_addr):
     bytes_str = map('{:02x}'.format, bytes_addr)
     return ':'.join(bytes_str).upper()
 
-
+if __name__ == "__main__":
+    main()
 
 
 
